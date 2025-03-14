85ee93112e1e677e69340635a8692b0e2ae75081
ahikmatyar18 <abdullah181003@gmail.com>
2025-03-14 10:19:35 +0700

    create new file

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModelProviderFactory.kt
@@ -0,0 +1,18 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package com.androiddevs.mvvmnewsapp.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+
+class NewsViewModelProviderFactory(
+    val newsRepository: NewsRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NewsViewModel::class.java)) {
+            return NewsViewModel(newsRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+}
